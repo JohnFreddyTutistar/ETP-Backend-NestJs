159a8568c204ea75f60c5bae0761e026e0069dba
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-04-07 10:04:58 -0500

    eliminando comentarios de app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,11 @@ import { ConfigModule } from '@nestjs/config';
     EdutrackModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host:  process.env.DB_HOST, //'localhost',
-      port: parseInt(process.env.DB_PORT || '5432', 10), //5432,
-      username: process.env.DB_USER, //'postgres',
-      password: process.env.DB_PASSWORD, //'123456',
-      database:  process.env.DB_NAME, //'newdb',
+      host:  process.env.DB_HOST, 
+      port: parseInt(process.env.DB_PORT || '5432', 10), 
+      username: process.env.DB_USER, 
+      password: process.env.DB_PASSWORD, 
+      database:  process.env.DB_NAME, 
       autoLoadEntities: true,
       synchronize: true
     }),
