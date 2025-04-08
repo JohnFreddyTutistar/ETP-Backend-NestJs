6c780bdaab385ee1cff1f5c74c104dfe88c124a7
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-04-08 14:56:14 -0500

    se crean nuevos enumerables para nuevas columnas en entidades

---
+++ src/edutrack/Enum/academic-schedule.enum.ts
@@ -0,0 +1,8 @@
+export enum academicSchedule {
+  DAYTIME = 'DIURNA', // Schedule daytime
+  EVENING = 'NOCTURNA', // Schedule nocturna
+  MIXED = 'MIXTA', // Schedule mixed
+  SATURDAY = 'SABATINA', // Schedule saturday
+  SUNDAY = 'DOMINICAL', // Schedule sunday
+  FLEXIBLE = 'FLEXIBLE', // Schedule flexible
+}
--- src/edutrack/Enum/status.enum.ts
+++ src/edutrack/Enum/status.enum.ts
@@ -1,6 +1,12 @@
 export enum statusApplicant {
-    APPROVED = 'APROVADO',
-    REVISION = 'EN REVISIÓN',
-    GIVENUP = 'DESISTIDO',
-    REJECTED = 'RECHAZADO',
-}   
+  APPROVED = 'APROVADO',
+  REVISION = 'EN REVISIÓN',
+  GIVENUP = 'DESISTIDO',
+  REJECTED = 'RECHAZADO',
+}
+
+export enum statusDocument {
+  PENDING = 'PENDIENTE',
+  APPROVED = 'APROVADO',
+  REJECTED = 'RECHAZADO',
+}
